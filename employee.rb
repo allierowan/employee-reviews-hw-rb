c94b7ea3f89f524c5c73802996c817b534e6f129
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-20 15:45:24 -0400

    created add_review method which passed test

--- employee.rb
+++ employee.rb
@@ -1,12 +1,19 @@
 class Employee
 
-  attr_reader :name, :email, :phone, :salary
+  attr_reader :name, :email, :phone
+
+  attr_accessor :review, :salary
 
   def initialize(name, email, phone, salary)
     @name = name
     @email = email
     @phone = phone
     @salary = salary
+    @review = ""
+  end
+
+  def add_review(text)
+    self.review = text
   end
 
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -9,8 +9,14 @@ class EmployeeTest <Minitest::Test
   end
 
   def test_create_employee
-    employee_1 = Employee.new( "Kvothe Kingkiller", "kvothe@theuniversity.com", "777-555-7777", 40000)
-    assert_equal "Kvothe Kingkiller", employee_1.name
+    kvothe = Employee.new( "Kvothe Kingkiller", "kvothe@theuniversity.com", "777-555-7777", 40000)
+    assert_equal "Kvothe Kingkiller", kvothe.name
+  end
+
+  def test_employee_review
+    kvothe = Employee.new( "Kvothe Kingkiller", "kvothe@theuniversity.com", "777-555-7777", 40000)
+    kvothe.add_review("He has stolen princesses back from sleeping barrow kings. He has burned down the town of Trebon. He has spent the night with Felurian and left with both his sanity and his life. He was expelled from the University at a younger age than most people are allowed in. He tread paths by moonlight that others fear to speak of during day. He has talked to Gods, loved women, and written songs that make the minstrels weep. You may have heard of him.")
+    assert_equal "He has stolen princesses back from sleeping barrow kings. He has burned down the town of Trebon. He has spent the night with Felurian and left with both his sanity and his life. He was expelled from the University at a younger age than most people are allowed in. He tread paths by moonlight that others fear to speak of during day. He has talked to Gods, loved women, and written songs that make the minstrels weep. You may have heard of him.", kvothe.review
   end
 
 end
