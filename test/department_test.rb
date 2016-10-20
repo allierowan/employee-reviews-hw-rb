8a49221e73af2ba447f0bace08ad8dc221952d98
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-20 15:12:44 -0400

    added test for get department salary

--- test/department_test.rb
+++ test/department_test.rb
@@ -25,4 +25,13 @@ class DepartmentTest <Minitest::Test
     assert aurors.employees[0].is_a? Employee
   end
 
+  def test_get_department_salary
+    aurors = Department.new("Aurors")
+    harry_potter = Employee.new("Harry Potter", "hpotter@hogwarts.com", "303-555-1234", 60000)
+    ron_weasley = Employee.new("Ronald Weasley", "rweasley@hogwarts.com", "303-555-9393", 59999)
+    aurors.add_employee(harry_potter)
+    aurors.add_employee(ron_weasley)
+    assert_equal 119999, aurors.total_salary
+  end
+
 end
